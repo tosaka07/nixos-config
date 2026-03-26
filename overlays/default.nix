be10a6601a1f44be3454ce035763c58107c8f2dc
tosaka07 <tosakaup@gmail.com>
2026-03-26 14:25:47 +0900

    fix(direnv): workaround CGO linkmode build failure

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,14 @@
 final: prev: {
   gwq = prev.callPackage ./gwq { };
   octorus = prev.callPackage ./octorus { };
+
+  # Fix: https://github.com/NixOS/nixpkgs/pull/502769
+  direnv = prev.direnv.overrideAttrs (old: {
+    makeFlags = (old.makeFlags or [ ]) ++ [ "CGO_ENABLED=0" ];
+    postPatch =
+      (old.postPatch or "")
+      + ''
+        substituteInPlace GNUmakefile --replace-fail '-linkmode=external' ""
+      '';
+  });
 }
